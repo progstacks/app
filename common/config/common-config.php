798ca133b4842821995513ffa5eba28666dd5faa
progstacks <admin@programmingstacks.com>
2018-05-19 15:34:03 +0800

    removed dev and prod-config files

--- common/config/common-config.php
+++ common/config/common-config.php
@@ -2,4 +2,9 @@
 $common=[
     'env'=>'dev',
     'name'=>'Red Knot Framework',
+    'defaults'=>[
+        'router'=>['class'=>'rk\\core\\Router'],
+        'controller'=>['class'=>'common\\controller\\SiteController'],
+        'site404'=>['class'=>'common\\controller\\Site404Controller']
+    ]
 ];
--- environment/prod/index.php
+++ environment/prod/index.php
@@ -2,6 +2,8 @@
 require_once 'config\prod-config.php';
 require_once '..\..\common\config\common-config.php';
 require_once '..\..\..\vendor\rk\autoloader.php';
+
 $config = array_merge($common,$environment);
+$config['base_path'] = __DIR__;
 $app = new rk\App($config);
 $app->start();
